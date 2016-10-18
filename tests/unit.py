2ffdea6a575a49009d8acf8b3f08ef380b4e03ed
Angad Gill <angadsinghgill@gmail.com>
2016-10-18 10:45:33 -0700

    Disk class and first unit test :D

--- tests/unit.py
+++ tests/unit.py
@@ -1,1 +1,37 @@
 """ All unit tests for the file system """
+
+import os
+import unittest
+
+from unix_fs import device_io
+
+""" device_io.py """
+
+class DiskClass(unittest.TestCase):
+
+    def setUp(self):
+        """ Executed before each test case """
+        b = bytearray([12, 12, 12])
+        path = 'temp_test_file'
+        f = device_io.Disk(path)
+        f.open()
+        f.write(b)
+        f.close()
+
+    def tearDown(self):
+        """ Executed after each test case """
+        path = 'temp_test_file'
+        os.remove(path)
+
+    def test_seek_read(self):
+        b = bytearray([12, 12, 12])
+        path = 'temp_test_file'
+        f = device_io.Disk(path)
+        f.open()
+        f.seek(0)
+        b2 = f.read(len(b))
+        f.close()
+        self.assertEqual(b, b2)
+
+if __name__ == '__main__':
+    unittest.main()
--- unix_fs/device_io.py
+++ unix_fs/device_io.py
@@ -1,1 +1,34 @@
 """ All low-level device IO classes and functions """
+
+import io
+
+class Disk(object):
+    """ Base class for writing to a raw disk """
+    def __init__(self, root):
+        self.root = root
+
+    def open(self):
+        # Open with binary mode with 0 buffering
+        try:
+            # Open root path in append binary mode 'rb+'
+            self._disk = io.open(self.root, 'rb+', buffering = 0)
+        except IOError:
+            # If root path does not exist, open in write binary mode
+            self._disk = io.open(self.root, 'wb', buffering = 0)
+
+    def close(self):
+        self._disk.close()
+
+    def read(self, n):
+        """ Read n bytes """
+        return self._disk.read(n)
+
+    def write(self, b):
+        """ Write bytearray b. Returns int n: number of bytes written """
+        n = self._disk.write(b)
+        return n  # number of bytes actually written
+
+    def seek(self, pos):
+        """ Seek to integer pos. Does not return anything."""
+        self._disk.seek(pos)
+
