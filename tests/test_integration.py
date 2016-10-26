ee4daf508660aa88fce251310c7023f79d6be1bd
Angad Gill <angadsinghgill@gmail.com>
2016-10-25 17:37:30 -0700

    Added command and dir test

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -7,29 +7,48 @@ import os
 MOUNTPOINT = 'tempfs_mountpoint'
 ROOT = 'tempfs_root'
 
-
 class TestFileSystem(unittest.TestCase):
     """ Base test class for file systems with helper functions """
     @staticmethod
-    def filename_to_path(filename):
+    def name_to_path(name):
         """ Convert filename to file path under MOUNTPOINT """
-        return os.path.join(MOUNTPOINT, filename)
+        return os.path.join(MOUNTPOINT, name)
 
     @staticmethod
     def create_file(filepath):
-        """ Helper function to create an empty file given absolute file path """
+        """ Helper function to create an empty file given absolute path """
         open(filepath, 'a').close()
 
     @staticmethod
     def delete_file(filepath):
-        """ Helper function to delete a file given absolute file path """
-        os.remove(filepath)  # cleanup so that all test are independent
+        """ Helper function to delete a file given absolute path """
+        os.remove(filepath)
 
     @staticmethod
     def file_exists(filepath):
-        """ Helper function to check if file exists given absolute file path """
+        """ Helper function to check if file exists given absolute path """
         return os.path.isfile(filepath)
 
+    @staticmethod
+    def create_dir(dirpath):
+        """ Helper function to create an empty directory given absolute path """
+        os.makedirs(dirpath)
+
+    @staticmethod
+    def delete_dir(dirpath):
+        """ Helper function to delete a directory given absolute path """
+        os.removedirs(dirpath)
+
+    @staticmethod
+    def dir_exists(dirpath):
+        """ Helper function to check if directory exists given absolute path """
+        return os.path.isdir(dirpath)
+
+    def assertCommandSuccessful(self, command):
+        """" Asserts that a command runs without error. Suppresses print from the command. """
+        success_response = 0
+        with open(os.devnull, 'w') as FNULL: # null file to redirect stdout
+            self.assertEqual(subprocess.call(command, shell=True, stdout=FNULL), success_response)
 
 class TestLoopback(TestFileSystem):
     @classmethod
@@ -53,15 +72,18 @@ class TestLoopback(TestFileSystem):
         for dir in [MOUNTPOINT, ROOT]:
             os.removedirs(dir)
 
+    def test_ls(self):
+        self.assertCommandSuccessful('ls')
+
     def test_file_create_delete(self):
-        filepath = self.filename_to_path('file1')
+        filepath = self.name_to_path('file1')
         self.create_file(filepath)
         self.assertTrue(self.file_exists(filepath))
         self.delete_file(filepath)
 
     def test_file_write_read(self):
         test_text = "this is the test text"
-        filepath = self.filename_to_path('file2')
+        filepath = self.name_to_path('file2')
         # write to file
         with open(filepath, 'w') as f:
             f.write(test_text)
@@ -73,7 +95,7 @@ class TestLoopback(TestFileSystem):
 
     def test_num_file_create_delete(self):
         num = 10
-        filepaths = [self.filename_to_path('file_num_{}').format(i) for i in range(num)]
+        filepaths = [self.name_to_path('file_num_{}').format(i) for i in range(num)]
         # create
         for filepath in filepaths:
             self.create_file(filepath)
@@ -84,6 +106,11 @@ class TestLoopback(TestFileSystem):
         for filepath in filepaths:
             self.delete_file(filepath)
 
+    def test_dir_create_delete(self):
+        dirpath = self.name_to_path('dir1')
+        self.create_dir(dirpath)
+        self.assertTrue(self.dir_exists(dirpath))
+        self.delete_dir(dirpath)
 
 if __name__ == '__main__':
     unittest.main()
