0e5ca0ebf535f1161d67695002b8f81cf5fc59ab
Angad Gill <angadsinghgill@gmail.com>
2016-11-03 18:23:35 -0700

    Can write to and read from a file

--- unix_fs/data_structures.py
+++ unix_fs/data_structures.py
@@ -153,6 +153,25 @@ class File(Inode):
     def __init__(self, device, index=None):
         super().__init__(itype=1, device=device, index=index)
 
+    def write(self, data):
+        block = DataBlock(self._device)
+        self.add_block_to_list(block)
+        block.__write__(data)
+
+    def read(self):
+        """ Reads and returns the first block, for now """
+        block = DataBlock(self._device, self.address_direct[0])
+        return block.__read__()
+
+    def add_block_to_list(self, block):
+        # Find the first spot to add the block index to
+        for i in range(len(self.address_direct)):
+            if self.address_direct[i] == 0:
+                self.address_direct[i] = block.index
+                break
+        else:
+             raise Exception('File full')
+
 
 class Directory(Inode):
     def __init__(self, device, index=None):
